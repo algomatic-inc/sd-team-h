ec82e6e30b14cfebdfec2961f0a25590e661421e
Shoji Ogura <ogurash@gmail.com>
2024-11-28 08:44:22 +0900

    Add info window

--- route_finder/frontend/src/components/ui/RouteMap.tsx
+++ route_finder/frontend/src/components/ui/RouteMap.tsx
@@ -2,11 +2,13 @@ import { Place, Route } from "@/lib/SearchResponse";
 import {
   AdvancedMarker,
   APIProvider,
+  InfoWindow,
   Map,
   Pin,
+  useAdvancedMarkerRef,
   useMap,
 } from "@vis.gl/react-google-maps";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Location } from "@/lib/SearchResponse";
 import { PinWithTextIcon } from "./icons/PinWithTextIcon";
 
@@ -91,6 +93,51 @@ function PlaceList({ places }: PlaceListProps): JSX.Element {
   );
 }
 
+type MarkerWithInfoWindowProps = {
+  location: Location;
+  title: string;
+  description: string;
+  pinText: string;
+};
+
+// See https://visgl.github.io/react-google-maps/docs/api-reference/components/info-window
+function MarkerWithInfoWindow({
+  location,
+  title,
+  description,
+  pinText,
+}: MarkerWithInfoWindowProps): JSX.Element {
+  const [markerRef, marker] = useAdvancedMarkerRef();
+  const [infoWindowShown, setInfoWindowShown] = useState(false);
+  const handleMarkerClick = useCallback(
+    () => setInfoWindowShown((isShown) => !isShown),
+    []
+  );
+  const handleClose = useCallback(() => setInfoWindowShown(false), []);
+
+  return (
+    <>
+      <AdvancedMarker
+        ref={markerRef}
+        position={{
+          lat: location.latitude,
+          lng: location.longitude,
+        }}
+        onClick={handleMarkerClick}
+      >
+        <Pin glyph={pinText} glyphColor={"white"} />
+      </AdvancedMarker>
+
+      {infoWindowShown && (
+        <InfoWindow anchor={marker} onClose={handleClose}>
+          <h2 className="font-lg">{title}</h2>
+          <p>{description}</p>
+        </InfoWindow>
+      )}
+    </>
+  );
+}
+
 type RouteMapProps = {
   /** Route to display */
   route: Route;
@@ -137,18 +184,19 @@ export function RouteMap({
           lng: aggregatedLatLng.longitude / route.places.length,
         };
 
-  const mapRestriction =
-    southWestBound != null && northEastBound != null
-      ? {
-          latLngBounds: {
-            north: northEastBound.latitude,
-            south: southWestBound.latitude,
-            east: northEastBound.longitude,
-            west: southWestBound.longitude,
-          },
-          strictBounds: true,
-        }
-      : null;
+  // const mapRestriction =
+  //   southWestBound != null && northEastBound != null
+  //     ? {
+  //         latLngBounds: {
+  //           north: northEastBound.latitude,
+  //           south: southWestBound.latitude,
+  //           east: northEastBound.longitude,
+  //           west: southWestBound.longitude,
+  //         },
+  //         strictBounds: true,
+  //       }
+  //     : null;
+  const mapRestriction = null;
   return (
     <div className="flex flex-row gap-8">
       <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY ?? ""}>
@@ -167,14 +215,13 @@ export function RouteMap({
         />
         <GeoJsonLoader geoJson={route.pathGeoJson} />
         {route.places.map((place, index) => (
-          <AdvancedMarker
-            position={{
-              lat: place.location.latitude,
-              lng: place.location.longitude,
-            }}
-          >
-            <Pin glyph={getPinText(index)} glyphColor={"white"} />
-          </AdvancedMarker>
+          <MarkerWithInfoWindow
+            key={index}
+            location={place.location}
+            title={place.name}
+            description={place.description}
+            pinText={getPinText(index)}
+          />
         ))}
       </APIProvider>
     </div>
