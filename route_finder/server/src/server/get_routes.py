dc4da3bcc854a397593892d6a0f21391b5450550
yendoooo <yendoooo1004@gmail.com>
2024-11-27 14:53:47 +0900

    feat: handle exception

--- route_finder/server/src/server/get_routes.py
+++ route_finder/server/src/server/get_routes.py
@@ -1,4 +1,11 @@
+import logging
+
 from sqlalchemy import text
+
+
+_logger = logging.getLogger(__name__)
+
+MAX_RETRY_COUNT = 5
 
 
 def get_routes(
@@ -37,26 +44,35 @@ def get_routes(
         """
     )
 
-    response = db.session.execute(
-        sql,
-        {
-            "weight_length": weight_length,
-            "weight_green_index": weight_green_index,
-            "weight_water_index": weight_water_index,
-            "weight_shade_index": weight_shade_index,
-            "weight_slope_index": weight_slope_index,
-            "weight_road_safety": weight_road_safety,
-            "weight_isolation": weight_isolation,
-            "weight_landmarks": weight_landmarks,
-            "landmarks": landmarks,
-            "start_lat": start_lat,
-            "start_lon": start_lon,
-            "end_lat": end_lat,
-            "end_lon": end_lon,
-        },
-    )
+    for retry_count in range(MAX_RETRY_COUNT):
+        try:
+            response = db.session.execute(
+                sql,
+                {
+                    "weight_length": weight_length,
+                    "weight_green_index": weight_green_index,
+                    "weight_water_index": weight_water_index,
+                    "weight_shade_index": weight_shade_index,
+                    "weight_slope_index": weight_slope_index,
+                    "weight_road_safety": weight_road_safety,
+                    "weight_isolation": weight_isolation,
+                    "weight_landmarks": weight_landmarks,
+                    "landmarks": landmarks,
+                    "start_lat": start_lat,
+                    "start_lon": start_lon,
+                    "end_lat": end_lat,
+                    "end_lon": end_lon,
+                },
+            )
+            row = response.fetchone()
 
-    row = response.fetchone()
+            if not row:
+                raise Exception("No route found.")
+        except Exception as e:
+            _logger.error(f"Failed to get routes. {e=}")
+            if retry_count >= MAX_RETRY_COUNT:
+                raise e
+
     route_info: str = row[0]
     landmarks_info: str | None = row[1] if len(row) > 1 else None
 
